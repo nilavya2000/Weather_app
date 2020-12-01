136eb1727745934aad33ab27e37c1bde5e97bccf
NILAVYA DAS <dnilavya@gmail.com>
2020-12-01 18:50:02 +0000

    added_how_to_add_or_delete_city

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 import requests
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] : 'sqlite:///weather.db' 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///weather.db' 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db=SQLAlchemy(app)
 
@@ -11,21 +11,61 @@ class city(db.Model):
 	id = db.Column(db.Integer, primary_key=True)
 	name = db.Column(db.String(50), nullable=False)
 
-@app.route('/')
-def index():
+def g_w(c):
 	url='http://api.openweathermap.org/data/2.5/weather?q={}&appid=da607fdc1833d3f1052c37181b2c452c'
-	city='siliguri'
-	r= requests.get(url.format(city)).json()
-	
+	r= requests.get(url.format(c)).json()
+	return r
 
-	weather = {
-		'city' : city,
-		'temperature' : r['main']['temp'],
-		'description' : r['weather'][0]['description'],
-		'icon' : r['weather'][0]['icon'],
-	}
-	print(weather)
-	return render_template('index1.html', weather=weather)
+
+@app.route('/', methods=['GET','POST'])
+def index():
+	e_r=''
+	if request.method == 'POST':
+		new_city = request.form.get('city')
+		if new_city:
+			e_city = city.query.filter_by(name=new_city).first()
+
+			if not e_city:
+				n_d_obj = g_w(new_city)
+				if n_d_obj['cod']==200:
+					new_city_obj = city(name=new_city)
+					db.session.add(new_city_obj)
+					db.session.commit()
+				else:
+					e_r='city doesnot exist in the world '
+			else:
+				e_r='City already exist. '
+
+
+	cities = city.query.all()
+	# url='http://api.openweathermap.org/data/2.5/weather?q={}&appid=da607fdc1833d3f1052c37181b2c452c'
+	
+	weather_data=[]
+
+
+	for c in cities:
+		r= g_w(c.name)
+
+		weather = {
+			'city' : c,
+			'temperature' :round( r['main']['temp'] - 273.15),
+			'humidity': r['main']['humidity'],
+			'description' : r['weather'][0]['description'],
+			'icon' : r['weather'][0]['icon'],
+		}
+		
+		weather_data.append(weather)
+
+	return render_template('index1.html', weather_data=weather_data)
+
+@app.route('/delete/<name>/')
+def d_city(name):
+	c_name=city.query.filter_by(name=name)
+	
+	db.session.delete(c_name)
+	db.session.commit()
+
+	return redirect('/')
 
 if __name__ == '__main__':
    app.run(debug=True) 
